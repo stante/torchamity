6ffd8448edd12d9e59485b036f69e760454c9af7
Alexander Stante <stante@gmail.com>
2022-11-04 22:54:32 +0100

    Fix multiple bugs in Learner class

--- torchamity/learner.py
+++ torchamity/learner.py
@@ -11,32 +11,35 @@ class Learner:
         self.loss_fn = loss_fn
         self.model = model
 
-    def fit(self, epochs, val_metrics=None):
+    def fit(self, epochs, val_metrics=[]):
         result = {'loss': []}
+        for metric in val_metrics:
+            result.setdefault(metric.name, [])
+
         train_loader = DataLoader(self.train, batch_size=16, shuffle=True)
         val_loader = DataLoader(self.val, batch_size=16, shuffle=True)
         for epoch in tqdm(range(1, epochs + 1)):
             loss_epoch = 0
             self.model.train()
 
-        for x, y in train_loader:
-            self.optim.zero_grad()
-            preds = self.model(x)
-            loss = self.loss_fn(preds, y)
-            loss.backward()
-            self.optim.step()
-            loss_epoch += loss.item()
+            for x, y in train_loader:
+                self.optim.zero_grad()
+                preds = self.model(x)
+                loss = self.loss_fn(preds, y)
+                loss.backward()
+                self.optim.step()
+                loss_epoch += loss.item()
 
-        result['loss'].append(loss_epoch / len(train_loader))
+            result['loss'].append(loss_epoch / len(train_loader))
 
-        self.model.eval()
-        for x, y in val_loader:
-            preds = self.model(x)
+            self.model.eval()
+            for x, y in val_loader:
+                preds = self.model(x)
+
+                for metric in val_metrics:
+                    metric.update(torch.argmax(preds, dim=-1), y)
 
             for metric in val_metrics:
-                metric.update(torch.argmax(preds, dim=-1), y)
-
-        for metric in val_metrics:
-            result.get(metric.name, []).append(metric.result())
+                result[metric.name].append(metric.result())
 
         return result

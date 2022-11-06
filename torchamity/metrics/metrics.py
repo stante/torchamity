bfea16d7d670a8e87cf21aa8f8f35ad1ef48d9ac
Alexander Stante <stante@gmail.com>
2022-11-06 20:04:43 +0100

    Add mean class as base class for metrics

--- torchamity/metrics/metrics.py
+++ torchamity/metrics/metrics.py
@@ -1,20 +1,46 @@
-class Accuracy:
-    """Calculates accuracy metric.
+class Mean:
+    """Generiic class that calculates the mean.
     """
-    def __init__(self, name='acc'):
+    def __init__(self, name):
         self.name = name
         self.reset()
 
-    def update(self, y_true, y_pred):
-        self.total += (y_true == y_pred).float().mean().item()
-        self.count += 1
+    def update(self, *args):
+        raise NotImplementedError("Must be implemented in subclass.")
 
-    def result(self):
+    def result(self, reset=True):
         if self.count == 0:
             return 0
         else:
-            return self.total / self.count
+            total = self.total
+            count = self.count
+            if reset:
+                self.reset()
+                
+            return total / count
 
     def reset(self):
         self.total = 0
         self.count = 0
+
+
+class Loss(Mean):
+    """Calcualtes the loss metric.
+    """
+    def __init__(self, name='loss'):
+        super().__init__(name)
+
+    def update(self, loss):
+        self.total += loss
+        self.count += 1
+
+
+class Accuracy(Mean):
+    """Calculates accuracy metric.
+    """
+    def __init__(self, name='acc'):
+        super().__init__(name)
+
+    def update(self, y_true, y_pred):
+        self.total += (y_true == y_pred).float().mean().item()
+        self.count += 1

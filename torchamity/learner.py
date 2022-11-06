e18464ab3db12bf381c4e5484e11f82caa408fad
Alexander Stante <stante@gmail.com>
2022-11-06 20:25:46 +0100

    Restructure fit method

--- torchamity/learner.py
+++ torchamity/learner.py
@@ -12,36 +12,53 @@ class Learner:
         self.model = model
 
     def fit(self, epochs, metrics=[], val_metrics=[]):
-        result = {'loss': []}
+        def _train_batch(x, y):
+            self.optim.zero_grad()
+            preds = self.model(x)
+            loss = self.loss_fn(preds, y)
+            loss.backward()
+            self.optim.step()
 
-        for metric in metrics:
-            result.setdefault(metic.name, [])
+            for metric in metrics:
+                metric.update(torch.argmax(preds, dim=-1), y)
             
-        for metric in val_metrics:
-            result.setdefault(metric.name, [])
+            return loss.item()
+
+        def _val_batch(x, y):
+            preds = self.model(x)
+
+            for metric in val_metrics:
+                metric.update(torch.argmax(preds, dim=-1), y)
+
+        def _prepare_result():
+            result = {'loss': []}
+
+            for metric in metrics:
+                result.setdefault(metic.name, [])
+            
+            for metric in val_metrics:
+                result.setdefault(metric.name, [])
+
+            return result
+            
+        result = _prepare_result()
 
         train_loader = DataLoader(self.train, batch_size=16, shuffle=True)
         val_loader = DataLoader(self.val, batch_size=16, shuffle=True)
+
         for epoch in tqdm(range(1, epochs + 1)):
             loss_epoch = 0
             self.model.train()
 
             for x, y in train_loader:
-                self.optim.zero_grad()
-                preds = self.model(x)
-                loss = self.loss_fn(preds, y)
-                loss.backward()
-                self.optim.step()
-                loss_epoch += loss.item()
+                loss = _train_batch(x, y)
+                loss_epoch += loss
 
             result['loss'].append(loss_epoch / len(train_loader))
 
             self.model.eval()
             for x, y in val_loader:
-                preds = self.model(x)
-
-                for metric in val_metrics:
-                    metric.update(torch.argmax(preds, dim=-1), y)
+                _val_batch(x, y)
 
             for metric in val_metrics:
                 result[metric.name].append(metric.result())
